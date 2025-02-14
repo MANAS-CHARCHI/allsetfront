8b43b61efa9e62845ebf466eeb88df08b3c53fa2
Manas Charchi <charchimanas2018@gmail.com>
2025-02-15 00:36:16 +0530

    form UI done0.2

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+import Header from "../components/lock-header";
+import { ReactNode } from "react";
+import Footer from "../components/footer";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <div className="flex-grow">{children}</div>
+        <Footer />
+      </div>
+    </>
+  );
+}
