16b1c7954e2878640462a9e4f0c6bdb408224b67
Manas Charchi <charchimanas2018@gmail.com>
2025-02-16 01:33:39 +0530

    front setup 16 feb

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,5 @@
 import LockHeader from "../components/lock-header";
+import Footer from "../components/footer";
 
 export default function AuthLayout({
   children,
@@ -10,6 +11,7 @@ export default function AuthLayout({
       <div className=" antialiased flex flex-col min-h-screen">
         <LockHeader />
         <div className="flex-grow">{children}</div>
+        <Footer />
       </div>
     </div>
   );
